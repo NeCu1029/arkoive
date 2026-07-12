297833ccb20a5515cae268ed7531349326c63ae6
NeCu1029 <necu1029@gmail.com>
2026-07-12 23:22:32 +0900

    회원가입 구현

---
+++ flaskr/user.py
@@ -0,0 +1,84 @@
+# 회원가입, 로그인, 마이 페이지
+
+from flask import Blueprint, flash, render_template, redirect, request, session, url_for
+from flask_login import login_user, login_required, logout_user
+from bs4 import BeautifulSoup
+from random import randint
+import requests
+from .util import bc, db, User
+
+user_bp = Blueprint("user_bp", __name__)
+
+
+@user_bp.route("/register1", methods=["GET", "POST"])
+def register1():
+    if request.method == "GET":
+        return render_template("register1.html")
+
+    username = request.form.get("username").strip()  # type: ignore
+    if User.query.filter_by(username=username).first():
+        flash("이미 존재하는 아이디입니다.")
+        return redirect(url_for("user_bp.register1"))
+
+    code = []
+    for _ in range(10):
+        code.append(chr(randint(97, 122)))
+
+    session["register_username"] = username
+    session["register_code"] = code
+    session["register_verified"] = False
+    return redirect(url_for("user_bp.register2"))
+
+
+@user_bp.route("/register2", methods=["GET", "POST"])
+def register2():
+    if "register_username" not in session:
+        flash("회원가입 세션이 만료되었습니다. 다시 시도하세요.")
+        return redirect(url_for("user_bp.register1"))
+    username = session["register_username"]
+    code = session["register_code"]
+
+    if request.method == "GET":
+        return render_template("register2.html", username=username, code=code)
+
+    response = requests.get(f"https://koistudy.net/user_profile?id={username}")
+    soup = BeautifulSoup(response.text, "html.parser")
+    h1 = soup.select_one("div.profile-container h1").get_text()  # type: ignore
+    if "(" in h1:
+        name = h1[h1.find("(") + 1 : -8]
+    else:
+        name = ""
+
+    if code == name:
+        session["register_verified"] = True
+        return redirect(url_for("user_bp.register3"))
+
+    flash("계정 인증에 실패했습니다. 다시 시도하세요.")
+    return redirect(url_for("user_bp.register2"))
+
+
+@user_bp.route("/register3", methods=["GET", "POST"])
+def register3():
+    if "register_username" not in session:
+        flash("회원가입 세션이 만료되었습니다. 다시 시도하세요.")
+        return redirect(url_for("user_bp.register1"))
+    if not session.get("verified"):
+        flash("계정 인증에 실패했습니다. 다시 시도하세요.")
+        return redirect(url_for("user_bp.register2"))
+
+    if request.method == "GET":
+        return render_template("register3.html")
+
+    password = request.form["password"]
+    hash = bc.generate_password_hash(password).decode("utf-8")
+
+    user = User(username=session["register_username"], password=hash)  # type: ignore
+    db.session.add(user)
+    db.session.commit()
+
+    session.pop("register_username", None)
+    session.pop("register_code", None)
+    session.pop("verified", None)
+
+    flash("회원가입이 완료되었습니다.")
+    return redirect("/login")
---
+++ flaskr/util.py
@@ -0,0 +1,14 @@
+# 잡다한 것들 저장
+
+from flask_bcrypt import Bcrypt
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+bc = Bcrypt()
+db = SQLAlchemy()
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(100), unique=False, nullable=False)
