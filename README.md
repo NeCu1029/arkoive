3e560c7c4f380434c8137f685e2388a7eb2e0650
NeCu1029 <necu1029@gmail.com>
2026-08-02 00:42:47 +0900

    문제 리스트 페이지 보완, 어드민 페이지 제작 및 __init__.py에 통합, run.cmd 삭제

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ KOISTUDY의 문제 난이도와 유저 레이팅을 제공하는 서비스
 ## flask 관련
 
 flask
+flask-admin
 flask-bcrypt
 flask-login
 flask-sqlalchemy
@@ -14,4 +15,9 @@ flask-sqlalchemy
 ## 기타
 
 bs4
+python-dotenv
 requests
+
+# localhost 실행 방법
+
+cmd에서 `flask run` 입력
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import abort, Flask, render_template
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 import os
+from .prob import prob_bp
 from .user import user_bp
 from .util import bc, db, manager, User
 
@@ -8,6 +11,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.register_blueprint(user_bp)
+app.register_blueprint(prob_bp)
 
 bc.init_app(app)
 db.init_app(app)
@@ -16,6 +20,18 @@ with app.app_context():
 manager.init_app(app)
 manager.login_view = "user_bp.login"  # type: ignore
 manager.login_message = "로그인 상태가 아닙니다."
+
+
+class SecureModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.username == "arkoive"
+
+    def inaccessible_callback(self):
+        abort(403)
+
+
+admin = Admin(app, name="내 관리자 페이지")
+admin.add_view(SecureModelView(User, db.session))
 
 
 @manager.user_loader
@@ -33,4 +49,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
